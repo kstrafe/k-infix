131fdcaef7989d389855028dce8b594ca846f214
Kevin Robert Stravers <macocio@gmail.com>
2017-11-19 17:24:38 +0100

    Add memoize to build deps

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 (define collection "k-infix")
 (define deps '("base"
                "rackunit-lib"))
-(define build-deps '("scribble-lib" "racket-doc"))
+(define build-deps '("scribble-lib" "racket-doc" "memoize"))
 (define scribblings '(("scribblings/k-infix.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.1")
