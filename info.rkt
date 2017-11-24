07ed4c23905ea8b2b85a5f321d56ad038170766f
Kevin Robert Stravers <macocio@gmail.com>
2017-11-24 01:44:07 +0100

    Add sandbox-lib to build-deps

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 (define collection "k-infix")
 (define deps '("base"
                "rackunit-lib"))
-(define build-deps '("scribble-lib" "racket-doc" "memoize"))
+(define build-deps '("sandbox-lib" "scribble-lib" "racket-doc" "memoize"))
 (define scribblings '(("scribblings/k-infix.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "1.0")
