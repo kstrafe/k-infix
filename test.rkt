36d6d794c3d708eeac5d7f49ae9b5412cb869826
fsh <tristesse@gmail.com>
2025-07-12 03:18:40 +0200

    Fix handling of trailing postfix (#10)
    * Fix handling of trailing postfix
    
    * Some more unary test cases

--- test.rkt
+++ test.rkt
@@ -1,6 +1,6 @@
 #lang racket/base
 
-(require "main.rkt" rackunit syntax/parse/define (for-syntax racket/base))
+(require "main.rkt" "define.rkt" rackunit syntax/parse/define (for-syntax racket/base))
 
 (define-syntax-parser check-expr-results
   #:datum-literals (:)
@@ -79,3 +79,31 @@
   (1      : (- 1 + * - - 2))
   (-1/2   : (- 1 + * - / - 2))
   )
+
+(define e? check-equal?)
+
+(test-case "unary"
+  ; naive non-tail-call factorial
+  (define (! n)
+    (cond
+      [(< n 0) (error "negative argument")]
+      [(zero? n) 1]
+      [else (* n (! (sub1 n)))]))
+
+  ; factorial ! has higher precedence than *
+  (define-$+ $ (! 110 left -110))
+
+  ; the algorithm doesn't really distinguish between postfix and prefix.
+  (e? 24 ($ 4 !))
+  (e? 24 ($ ! 4))
+
+  (e? 2/3 ($ 1 - 3 /)) ; unary /x is 1/x
+
+  (e? 4 ($ - 2 + 3 !))
+  (e? -2 ($ - 3 ! + 4))
+  (e? -18 ($ - 3 * 3 !)) ; -(3 * (3!))
+  (e? 36 ($ - 3 ! * - 3 !))
+  (e? 720 ($ ! 3 !))
+  (e? 12 ($ 2 * 3 !)) ; 2 * (3!)
+  (e? 24 ($ 2 * 3 ! * 2)) ; 2 * (3!) * 2
+)
